8cc1859ed2a4eec094c0800488ae2f2b46f23198
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-15 11:20:43 -0500

    agregando estilos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 window.onload = function() {
-  document.getElementById('button').addEventListener('click', function() {
-    var showText = document.getElementById('text-publicado');
-    var myText = document.getElementById('textarea').value;
-    showText.innerHTML = myText;
-  });
+  var one = document.getElementById('button');
+  two = one.addEventListener('click', visuali);
 };
+  
+function visuali(event) {
+  var contenedor = document.querySelector('.visualizando');
+  var texto = document.formulario.text.value;
+  if (texto === 0) {
+    document.getElementById('button').disable = true;
+  } else {
+    contenedor.innerHTML = texto;
+    document.getElementById('button').style.backgroundColor = '#ddd';
+    document.formulario.text.value = '';
+  }
+}
+var tweet = document.formulario.text;
+tweet.addEventListener('keydown', function() {
+  document.getElementById('contadorNumeros').innerHTML = tweet.value.length;
+});
+
+
