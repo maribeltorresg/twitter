f976e63addaa1991e8d4ca60ea8d2c24cd2ef1a4
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-16 18:36:47 -0500

    versión 0.0.2

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function() {
   var button = document.getElementById('button');
   var textarea = document.getElementById('textarea');
   var tweets = document.getElementById('tweets');
+  var counter = document.getElementById('counter');
 
   button.addEventListener('click', function() {
     // creando un tweet
@@ -12,9 +13,21 @@ window.addEventListener('load', function() {
     tweet.textContent = textarea.value;
     // agregar el tweet creado en su contenedor llamado tweets (plural)
     tweets.appendChild(tweet);
-    
-    
   });
+
+  textarea.addEventListener('keyup', function() {
+    var text = textarea.value.trim();
+
+    if (text.length > 0 & text !== '') {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+    // contador regresiva
+    counter.textContent = 140 - text.length;
+  });
+
+  
 });
 
 
