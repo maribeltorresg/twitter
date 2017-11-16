737e99b9bc883216420c9f2516c1bc16496f42c7
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-16 13:08:23 -0500

    agregando estilos

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
-window.onload = function() {
-  var one = document.getElementById('button');
-  two = one.addEventListener('click', visuali);
-};
-  
-function visuali(event) {
-  var contenedor = document.querySelector('.visualizando');
-  var texto = document.formulario.text.value;
-  if (texto === 0) {
-    document.getElementById('button').disable = true;
-  } else {
-    contenedor.innerHTML = texto;
-    document.getElementById('button').style.backgroundColor = '#ddd';
-    document.formulario.text.value = '';
-  }
-}
-var tweet = document.formulario.text;
-tweet.addEventListener('keydown', function() {
-  document.getElementById('contadorNumeros').innerHTML = tweet.value.length;
+window.addEventListener('load', function() {
+  var boton = document.getElementById('boton');
+
+  boton.addEventListener('click', function() {
+    var tweetPublicado = document.getElementById('tweet-publicado');
+    var text = document.formulario.text.value;
+    
+    if (text === 0) {
+      document.getElementById('botton').disabled = true;
+    } else {
+      tweetPublicado.textContent = text;
+      document.formulario.text.value = '';
+    }
+  });
 });
+
+// window.onload = function() {
+//   var one = document.getElementById('button');
+//   two = one.addEventListener('click', visuali);
+// };
+  
+// function visuali(event) {
+//   var contenedor = document.querySelector('.visualizando');
+//   var texto = document.formulario.text.value;
+//   if (texto === 0) {
+//     document.getElementById('button').disable = true;
+//   } else {
+//     contenedor.innerHTML = texto;
+//     // document.getElementById('button').style.backgroundColor = '#ddd';
+//     document.formulario.text.value = '';
+//   }
+// }
+// var tweet = document.formulario.text;
+// tweet.addEventListener('keydown', function() {
+//   document.getElementById('contadorNumeros').innerHTML = tweet.value.length;
+// });
 
 
