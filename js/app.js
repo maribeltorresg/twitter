72f1793861f8331e9b58096ddc696210b729002e
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-16 16:59:30 -0500

    mejorando la primera versión

--- js/app.js
+++ js/app.js
@@ -1,38 +1,20 @@
 window.addEventListener('load', function() {
-  var boton = document.getElementById('boton');
+  var button = document.getElementById('button');
+  var textarea = document.getElementById('textarea');
+  var tweets = document.getElementById('tweets');
 
-  boton.addEventListener('click', function() {
-    var tweetPublicado = document.getElementById('tweet-publicado');
-    var text = document.formulario.text.value;
+  button.addEventListener('click', function() {
+    // creando un tweet
+    var tweet = document.createElement('div');
+    // agregando una clase 'card' en tweet
+    tweet.className = 'card';
+    // asignandole el valor de textarea en el tweet
+    tweet.textContent = textarea.value;
+    // agregar el tweet creado en su contenedor llamado tweets (plural)
+    tweets.appendChild(tweet);
     
-    if (text === 0) {
-      document.getElementById('botton').disabled = true;
-    } else {
-      tweetPublicado.textContent = text;
-      document.formulario.text.value = '';
-    }
+    
   });
 });
-
-// window.onload = function() {
-//   var one = document.getElementById('button');
-//   two = one.addEventListener('click', visuali);
-// };
-  
-// function visuali(event) {
-//   var contenedor = document.querySelector('.visualizando');
-//   var texto = document.formulario.text.value;
-//   if (texto === 0) {
-//     document.getElementById('button').disable = true;
-//   } else {
-//     contenedor.innerHTML = texto;
-//     // document.getElementById('button').style.backgroundColor = '#ddd';
-//     document.formulario.text.value = '';
-//   }
-// }
-// var tweet = document.formulario.text;
-// tweet.addEventListener('keydown', function() {
-//   document.getElementById('contadorNumeros').innerHTML = tweet.value.length;
-// });
 
 
